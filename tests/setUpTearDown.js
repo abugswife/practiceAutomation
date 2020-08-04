9023b2cee71b835844b944389d39811625381c46
Bri Davis <bridavis@Bris-iMac.local>
2020-08-04 13:07:01 -0500

    template

---
+++ tests/setUpTearDown.js
@@ -0,0 +1,7 @@
+module.exports = {
+    beforeEach: browser => {
+        browser.url('https://devmountain-qa.github.io/enter-wanted/1.4_Assignment/index.html')
+    },
+    after: browser => {
+        browser.end()
+    }
