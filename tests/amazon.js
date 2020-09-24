5da025fc821f1bef89492f27d1727b177f3523f1
Bri Davis <bridavis@Bris-iMac.local>
2020-09-23 19:18:39 -0500

    updates

--- tests/amazon.js
+++ tests/amazon.js
@@ -18,3 +18,32 @@ module.exports = {
             .verify.containsText('@results', 'Lego Star Wars')
     }
 }
+
+//Testing Captcha Chance
+
+module.exports = {
+    'Log into system - create order': function (browser) {
+      var currentIteration = 0,
+          iterationCount = 10;
+  
+      function runTest() {
+        browser
+          .url('https://www.apartmentlist.com/')
+          .click('a[href="/users/login"]')
+          .click('input[name="email"]')
+          .setValue('input[name="email"]', 'davisdbrianna@gmail.com')
+          .click('#password')
+          .setValue('#password', 'bdenisem0')
+          .click('#login-button')
+          browser.perform(function() {
+            if (++currentIteration < iterationCount) {
+              return runTest();
+            }
+  
+            browser.end(); // After passing 30 iterations end the session
+          });
+      }
+  
+      runTest();
+    }
+  };
